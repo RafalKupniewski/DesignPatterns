661cb9608d3f79da0b542ba33212d1d5a9c60de0
rkupniewski <rkupniewski@sigma.inf.ug.edu.pl>
2017-12-08 18:37:05 +0100

    prototypy

---
+++ 04Prototype/Students/2017/rkupniewski/prototype/Prototyp2/Prototyp2/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+public class Id
+{
+    public int Ida;
+
+    public Id(int Id)
+    {
+        this.Ida = Id;
+    }
+}
+
+public class Auto
+{
+    public string Marka;
+    public string Model;
+    public Id Id;
+
+    public Auto ShallowCopy()
+    {
+        return (Auto)this.MemberwiseClone();
+    }
+
+    public Auto DeepCopy()
+    {
+        Auto other = (Auto)this.MemberwiseClone();
+        other.Id = new Id(Id.Ida);
+        other.Marka = String.Copy(Marka);
+        other.Model = String.Copy(Model);
+        return other;
+    }
+}
+
+public class Example
+{
+    public static void Main()
+    {
+        // tworzenie instancji
+        Auto a1 = new Auto();
+        a1.Model = "Ford";
+        a1.Marka = "SMax";
+        a1.Id = new Id(1);
+
+        // kopia plytka
+        Auto a2 = a1.ShallowCopy();
+
+        // wyswietlamy
+        Console.WriteLine("   Auto1: ");
+
+        Console.WriteLine(a1.Model);
+        Console.WriteLine(a1.Marka);
+        Console.WriteLine(a1.Id.Ida);
+
+        Console.WriteLine("   Kopia plytka Auto1:");
+        Console.WriteLine(a2.Model);
+        Console.WriteLine(a2.Marka);
+        Console.WriteLine(a2.Id.Ida);
+
+        Console.ReadKey();
+        // zmiana wartosci
+        a1.Model = "Ford";
+        a1.Marka = "Mondeo";
+        a1.Id.Ida = 2;
+        Console.WriteLine("Zmiana wartosci w zrodlowej instancji");
+        Console.WriteLine("   Auto1: ");
+
+        Console.WriteLine(a1.Model);
+        Console.WriteLine(a1.Marka);
+        Console.WriteLine(a1.Id.Ida);
+
+        Console.WriteLine("   Kopia plytka Auto1:");
+        Console.WriteLine(a2.Model);
+        Console.WriteLine(a2.Marka);
+        Console.WriteLine(a2.Id.Ida);
+
+        Console.ReadKey();
+
+        // kopia gleboka
+        Auto a3 = a1.DeepCopy();
+
+        Console.WriteLine("   Kopia gleboka Auto3:");
+        Console.WriteLine(a3.Model);
+        Console.WriteLine(a3.Marka);
+        Console.WriteLine(a3.Id.Ida);
+
+        Console.ReadKey();
+
+        a1.Model = "Fiat";
+        a1.Marka = "Bravo";
+        a1.Id.Ida = 3;
+        Console.WriteLine("Zmiana wartosci w zrodlowej instancji");
+
+        Console.WriteLine("   Auto1: ");
+
+        Console.WriteLine(a1.Model);
+        Console.WriteLine(a1.Marka);
+        Console.WriteLine(a1.Id.Ida);
+
+        Console.WriteLine("   Kopia gleboka Auto3:");
+        Console.WriteLine(a3.Model);
+        Console.WriteLine(a3.Marka);
+        Console.WriteLine(a3.Id.Ida);
+
+        Console.ReadKey();
+    }
+
+
+}
---
+++ 04Prototype/Students/2017/rkupniewski/prototype/prototype1/prototype1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsExamples
+{
+    class PrototypePattern
+    {
+        static void Main(string[] args)
+        {
+            // Instancje i klonowanie
+            ConcretePrototype p1 = new ConcretePrototype("wartosc 1");
+            ConcretePrototype c1 = (ConcretePrototype)p1.Clone();
+            ConcretePrototype c3 = (ConcretePrototype)p1.DeepClone();
+            Console.WriteLine("Cloned P1: {0}", c1.Id);
+            Console.WriteLine("DeepCloned: {0}", c3.Id);
+            c1.id = "Wartosc 2";
+            Console.WriteLine("Cloned P1: {0}", c1.Id);
+            Console.WriteLine("DeepCloned: {0}", c3.Id);
+            c3.id = "Wartosc 4";
+            Console.WriteLine("Cloned P1: {0}", c1.Id);
+            Console.WriteLine("DeepCloned: {0}", c3.Id);
+            ConcretePrototype c2 = (ConcretePrototype)p1.Clone();
+            //c1.Id = "new";
+            Console.WriteLine("Cloned P1: {0}", c1.Id);
+            Console.WriteLine("DeepCloned: {0}", c3.Id);
+            Console.WriteLine("Cloned P2: {0}", c2.Id);
+            
+             
+            Console.Read();
+        }
+    }
+
+    // Prototyp 
+    abstract class Prototype
+    {
+        public string id;
+
+        // Konstruktor  
+        public Prototype(string id)
+        {
+            this.id = id;
+        }
+
+        // Wlasciwosc 
+        public string Id
+        {
+            get { return id; }
+        }
+
+        public abstract Prototype Clone();
+        public abstract Prototype DeepClone();
+    }
+
+    // Prototyp rozszerzenie
+    class ConcretePrototype : Prototype
+    {
+        // Konstruktor "konkretny"
+        public ConcretePrototype(string id)
+            : base(id)
+        {
+        }
+    
+        public override Prototype Clone()
+        {
+            // kopia plytka 
+            return (Prototype)this.MemberwiseClone();
+        }
+
+        public override Prototype DeepClone()
+        {
+            // kopia gleboka
+            Prototype other = (Prototype)this.MemberwiseClone();
+            other.id = String.Copy(id);          
+            return other;
+        }
+    }
+
+    public class ClientClass
+    {
+        private Prototype prototype = null;
+        ClientClass(Prototype prototype)
+        {
+            this.prototype = prototype;
+        }
+    }
+}
+
