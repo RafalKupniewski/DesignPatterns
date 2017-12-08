dbef7ac8be4c38017e69aa86b567d722bdd89063
rkupniewski <rkupniewski@sigma.inf.ug.edu.pl>
2017-12-08 18:32:10 +0100

    Factory

---
+++ 02Fabryka/Students/2017/rkupniewski/AbstractFactory/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+class MainApp
+{
+    public static void Main()
+    {
+        // Abstract factory #1 
+        AbstractFactory factory1 = new ConcreteFactory1();
+        Client c1 = new Client(factory1);
+        c1.Machine1();
+     
+        Console.ReadKey();
+        // Abstract factory #2 
+        AbstractFactory factory2 = new ConcreteFactory2();
+        Client c2 = new Client(factory2);
+        c2.Machine1();
+        Console.ReadKey();
+
+        AbstractFactory factory3 = new ConcreteFactory3();
+        Client c3 = new Client(factory3);
+        c3.Machine1();
+       
+
+        // Wait for user input 
+        Console.ReadKey();
+    }
+}
+
+// "AbstractFactory" 
+abstract class AbstractFactory
+{
+    public abstract AbstractProductA CreateProductA();
+    public abstract AbstractProductB CreateProductB();
+}
+
+// "ConcreteFactory1" 
+class ConcreteFactory1 : AbstractFactory
+{
+    public override AbstractProductA CreateProductA()
+    {
+        return new Workstation();
+    }
+    public override AbstractProductB CreateProductB()
+    {
+        return new OS_Linux();
+    }
+}
+
+// "ConcreteFactory2" 
+class ConcreteFactory2 : AbstractFactory
+{
+    public override AbstractProductA CreateProductA()
+    {
+        return new Server();
+    }
+    public override AbstractProductB CreateProductB()
+    {
+        return new OS_Windows();
+    }
+}
+
+// "ConcreteFactory3"
+class ConcreteFactory3 : AbstractFactory
+{
+    public override AbstractProductA CreateProductA()
+    {
+        return new Workstation();
+    }
+    public override AbstractProductB CreateProductB()
+    {
+        return new OS_Windows();
+    }
+}
+
+
+// "AbstractProductA" 
+abstract class AbstractProductA
+{
+    public abstract void Wytworz(AbstractProductB typ);
+}
+
+// "AbstractProductB" 
+abstract class AbstractProductB
+{
+    public abstract void Wytworz(AbstractProductA typ);
+}
+
+// "ProductA1" 
+class Workstation : AbstractProductA
+{
+    public override void Wytworz(AbstractProductB typ )
+    {
+        Console.WriteLine(this.GetType().Name);
+    }
+}
+
+// "ProductB1" 
+class OS_Linux : AbstractProductB
+{
+    public override void Wytworz(AbstractProductA typ)
+    {
+        Console.WriteLine(this.GetType().Name + " jest wytwarzany w wersji " + typ.GetType().Name);
+    }
+}
+
+// "ProductA2" 
+class Server : AbstractProductA
+{
+    public override void Wytworz(AbstractProductB typ)
+    {
+        Console.WriteLine(this.GetType().Name + typ.GetType().Name);
+    }
+}
+
+// "ProductB2" 
+class OS_Windows : AbstractProductB
+{
+    public override void Wytworz(AbstractProductA typ)
+    {
+        Console.WriteLine(this.GetType().Name + " jest wytwarzany w wersji " + typ.GetType().Name);
+    }
+}
+
+// "Client request product" 
+class Client
+{
+    private AbstractProductA TypeOS;
+    private AbstractProductB OS;
+    
+ 
+    // Constructor 
+    public Client(AbstractFactory factory)
+    {
+        TypeOS = factory.CreateProductA();
+        OS = factory.CreateProductB();
+          
+    }
+
+    public void Machine1()
+    {
+        OS.Wytworz(TypeOS);
+    }
+   
+}
---
+++ 02Fabryka/Students/2017/rkupniewski/Factory/ConsoleApplication5/fabryka.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bankomat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tworzenie instancji klasy w której znajduje się metoda do dokonania wypłaty
+            PaymentProcessor pre = new PaymentProcessor();
+            // Definiujemy operacje
+            Operacja operacja = new Operacja();
+            //operacja wypłaty 1
+            operacja.Wyplata = 5000;
+            // Dokonujemy wypłaty pierwszym sposobem.
+            pre.MakePayment(EPaymentMethod.EURONET, operacja);
+            Console.ReadKey();
+            //operacja wypłaty 2
+            operacja.Wyplata = 4000;
+            pre.MakePayment(EPaymentMethod.OTHER, operacja);
+            Console.ReadKey();
+        }
+    }
+    public enum EPaymentMethod
+    {
+        EURONET,
+        OTHER,
+    }
+    public class Operacja
+    {
+        public decimal Wyplata { get; set; }
+    }
+    public interface IPaymentGateway
+    {
+        void MakePayment(Operacja product);
+    }
+    public class OperatorOne : IPaymentGateway
+    {
+        public void MakePayment(Operacja product)
+        {
+            double prowizja = (float)product.Wyplata/100;
+            // Metoda to pozwala na dokonanie wypłaty za pomocą pierwszego sposobu
+            Console.WriteLine("Pierwszy rodzaj wypłaty kwota {0}, prowizja {1}", product.Wyplata, prowizja);
+        }
+    }
+    public class OperatorTwo : IPaymentGateway
+    {
+        public void MakePayment(Operacja product)
+        {
+            float prowizja = (float)product.Wyplata/20;
+            // Metoda to pozwala na dokonanie wypłaty za pomocą drugiego sposobu
+            Console.WriteLine("Drugi rodzaj wypłaty kwota {0}, prowizja {1}", product.Wyplata, prowizja);
+        }
+    }
+    public class PaymentGatewayFactory
+    {
+        public virtual IPaymentGateway CreatePaymentGateway(EPaymentMethod method, Operacja prod)
+        {
+            IPaymentGateway gateway = null;
+            switch (method)
+            {
+                case EPaymentMethod.EURONET:
+                    gateway = new OperatorOne();
+                    break;
+                case EPaymentMethod.OTHER:              
+                    gateway = new OperatorTwo();
+                    break;
+                default:
+                    break;
+            }
+            return gateway;
+        }
+    }
+    public class PaymentProcessor
+    {
+        IPaymentGateway gateway = null;
+        // Dokonywanie wypłaty
+        // Wywołanie metody CreatePaymentGateway(...) zwraca nam obiekt utworzony
+        // w zależności od wyboru rodzaju wypłaty przez klienta
+        public void MakePayment(EPaymentMethod method, Operacja product)
+        {
+            PaymentGatewayFactory factory = new PaymentGatewayFactory();
+            this.gateway = factory.CreatePaymentGateway(method, product);
+            this.gateway.MakePayment(product);
+        }
+    }
+}
