5243cf27639ada22bad6c4caa50373f2f4704b42
rkupniewski <rkupniewski@sigma.inf.ug.edu.pl>
2017-12-08 20:26:22 +0100

    Adapter

---
+++ 06Adapter/Students/2017/rkupniewski/Adapter/Adapter/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+class MainApp
+{
+    static void Main()
+    {
+        
+        // tworzymy adapter i wywolujemy request
+        Target target = new Adapter();
+        target.Request();
+
+        // guzik 
+        Console.Read();
+    }
+}
+
+// "pierwotna klasa target" 
+class Target
+{
+    public virtual void Request()
+    {
+        int a = 3;
+        string b = "alamakota";
+        Console.WriteLine("{0} x {1}", a, b);
+    }
+}
+
+// "rozszerzamy klase adapter o klase target i nadpisujemy metode request" 
+class Adapter : Target
+{
+    private Adaptee adaptee = new Adaptee();
+
+    public override void Request()
+    {
+        // wywolujemy request ale z klasy adapter ;-)
+        adaptee.SpecificRequest();
+    }
+}
+
+// "klasa adaper" 
+class Adaptee
+{
+    public void SpecificRequest()
+    {
+        string a = "dzien dobry";
+        int b = 8;
+        Console.WriteLine("{0} x {1}",  a ,b);
+    }
+}
